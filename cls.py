e5f8fcb38caab699ecefda48e5c6f5f7faa3eb58
DanCUWA <danc@DESKTOP-I535GEV.localdomain>
2024-09-16 15:33:26 +0800

    Added basic board initialisations and visualisation

--- cls.py
+++ cls.py
@@ -1,6 +1,8 @@
 import random
-PROB_NURSE = 0.2
-NUM_CLUSTERS = 3
+import pygame
+import time
+# The Person class acts as the primary Agent for 
+#   our scenario.
 class Person: 
     def __init__(self,prob_nurse): 
         self.nurse = False
@@ -24,7 +26,7 @@ class Person:
     def add_disease(self,disease): 
         self.diseases.append(disease)
         
-    
+# The Disease class acts as the secondary Agent for our scenario.
 class Disease:
     def __init__(self): 
         self.infectivity = random.randint(0,0.5)
@@ -32,23 +34,69 @@ class Disease:
     def infect_person(self,map): 
         map.get_random_person().add_disease(self)
 
+
+## The map acts as the environment for our scenario.
 class Map:
-    def __init__(self,size,k): 
+    def __init__(self,size,k,prob_nurse,prob_person): 
         self.size = size
+        self.prob_nurse = prob_nurse
+        self.prob_person = prob_person
         self.num_clusters = k
         self.people = list()
-        self.board = self.generate_array()
+        self.board =  [ [0] * self.size for i in range(self.size)  ]
+        self.generate_array()
 
     def generate_array(self): 
-        board = [self.size][self.size]
         for i in range(self.size): 
             for j in range(self.size):
-                pass
-
-    def add_person(self,i,j,cluster_props):
-        self.board[i][j] = Person(cluster_props)
+                if random.random() < self.prob_person:
+                    self.add_person(i,j,[0.2])
+                # pass
 
     def get_random_person(self): 
         return random.choice(self.people)
+    
+    
+    def add_person(self,i,j,cluster_props):
+        self.board[i][j] = Person(prob_nurse=self.prob_nurse)
 
-            
+    def get_map(self):
+        return self.board
+
+class Simulation: 
+
+    def __init__(self,board_size=100,num_clusters=4,prob_nurse=0.2,prob_person=0.4): 
+        self.board_size = board_size
+        self.num_clusters = num_clusters
+        self.prob_nurse = prob_nurse
+        self.prob_person = prob_person
+        self.map = Map(board_size,num_clusters,prob_nurse=prob_nurse,prob_person=prob_person)
+        self.disease_choices = list()
+
+
+    def add_disease_option(self): 
+        self.disease_choices.append(Disease())
+
+    def step(self): 
+        pass
+    
+    def view(self):
+        pygame.init()
+        screen_size = 750
+        size_offset = float(float(screen_size) / float(self.board_size)) * 1.1
+        print("Offset ", size_offset)
+        screen = pygame.display.set_mode([screen_size, screen_size])
+        screen.fill((255, 255, 255))
+        for i in range(self.board_size): 
+            for j in range(self.board_size):
+                x = i * size_offset
+                y = j * size_offset
+                # print("(",x,y,end=") , ")
+                pygame.draw.circle(screen, (100, 100, 100), (x,y), 3)
+        # while True:
+        pygame.display.flip()
+        time.sleep(10)
+        # pygame.quit()
+
+    def show_map(self): 
+        return self.map.get_map()
--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-
+from cls import *
 BOARDSIZE = 100
 NUM_CLUSTERS = 4
+PROB_NURSE = 0.2
+PROB_PERSON = 0.5 
 def main():
-    board = [BOARDSIZE][BOARDSIZE]
+    sim = Simulation(board_size=BOARDSIZE,num_clusters=NUM_CLUSTERS,
+                     prob_nurse=PROB_NURSE,prob_person=PROB_PERSON)
+    print(sim.show_map())
+    sim.view()
 if __name__ == "__main__": 
     main()
